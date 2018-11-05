75da8241d7029204837dd845758d03a69f6a19e4
Genievot <40867747+genievot@users.noreply.github.com>
2018-11-05 23:13:04 +0530

    Working with tabs
    Adding functionality to tabs so the old array can go empty and start with new one each time tab chaneges

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -11,8 +11,8 @@
   <q-page-container>
     <q-page>
       <q-tabs swipeable color="black" align="justify" v-model="selectedTab">
-        <q-tab label="Admin Board" slot="title" name="tab-1" icon="dashboard" />
-        <q-tab label="Student board" slot="title" name="tab-2" icon="developer_board" />
+        <q-tab label="Admin Board" slot="title" name="tab-1" icon="dashboard"  @select="openAdminBoard"/>
+        <q-tab label="Student board" slot="title" name="tab-2" icon="developer_board" @select="openStudentBoard"/>
       </q-tabs>
       <!-- // random string generator for urls https://helloacm.com/api/random/?n=128 -->
       <div class="row justify-center">
@@ -20,7 +20,7 @@
           <q-card-media>
             <!-- <img v-bind:src="book.Image" style="width: 400px; height:250px;"/> -->
             <div class="wrap" style="overflow: hidden;">
-            <object width="400px" height="250px" style="object-fit:cover" scrolling='no' v-bind:data="book.Image" frameborder="0" allowfullscreen></object>
+            <object width="400px" height="250px" style="object-fit:cover" v-bind:data="book.Image" frameborder="0"></object>
           </div>
           </q-card-media>
           <q-card-title>
@@ -41,21 +41,37 @@
 </template>
 
 <script>
-import {
-  openURL
-} from 'quasar'
-
 export default {
   name: 'MyLayout',
   data () {
     return {
       leftDrawerOpen: this.$q.platform.is.desktop,
-      books: [{}],
+      books: [],
       selectedTab: 'tab-1'
     }
   },
   methods: {
-    openURL
+    openStudentBoard () {
+      console.log('opening S board')
+      this.books = []
+      this.$studentref.on('value', (snapshoti) => {
+        console.log(snapshoti.val())
+        this.books = snapshoti.val()
+      }, function (errorObject) {
+        console.log('The read failed: ' + errorObject.code)
+      })
+    },
+    openAdminBoard () {
+      console.log('opening A board')
+      this.books = []
+      this.$bookref.on('value', (snapshot) => {
+        console.log(snapshot.val())
+        this.books = snapshot.val()
+        console.log(this.books[1]['Random Seed'])
+      }, function (errorObject) {
+        console.log('The read failed: ' + errorObject.code)
+      })
+    }
   },
   mounted () {
     // let book = null
@@ -63,11 +79,6 @@ export default {
       console.log(snapshot.val())
       this.books = snapshot.val()
       console.log(this.books[1]['Random Seed'])
-    }, function (errorObject) {
-      console.log('The read failed: ' + errorObject.code)
-    })
-    this.$studentref.on('value', (snapshoti) => {
-      console.log(snapshoti.val())
     }, function (errorObject) {
       console.log('The read failed: ' + errorObject.code)
     })
