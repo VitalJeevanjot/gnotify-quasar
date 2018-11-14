5be2a7d0354ec82e8f9dfa7445ced5fc8b05f334
Genievot <40867747+genievot@users.noreply.github.com>
2018-11-14 17:03:08 +0530

    Removed firebase warning
    Removed firebase of importing all modules...

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,5 +1,8 @@
 // import something here
-import Firebase from 'firebase'
+import Firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+import 'firebase/storage'
 let config = {
   apiKey: 'AIzaSyBuMrGyuh5z3yr6bSNZxCo2eU2zxudV47c',
   authDomain: 'webnoticeboard.firebaseapp.com',
